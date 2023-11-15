cb21b607f77387f713e5ecacdc777eae859f2eb6
irapanch <117355580+irapanch@users.noreply.github.com>
2023-11-15 12:03:45 +0200

    create app

---
+++ controllers/contacts/getById.js
@@ -0,0 +1,14 @@
+const contacts = require("../../models/contacts");
+
+const { RequestError } = require("../../helpers");
+
+const getById = async (req, res) => {
+  const { id } = req.params;
+  const result = await contacts.getById(id);
+  if (!result) {
+    throw RequestError(404, "Not found");
+  }
+  res.status(200).json(result);
+};
+
+module.exports = getById;
